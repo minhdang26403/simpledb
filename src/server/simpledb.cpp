70fcd69e1d72cf9a93ae4eb406731cedcc1c7951
Dang Truong <truongminhdang26403@gmail.com>
2024-01-24 16:55:21 -0500

    Add buffer manager to the database engine

--- src/server/simpledb.cpp
+++ src/server/simpledb.cpp
@@ -1,8 +1,19 @@
 #include "server/simpledb.h"
+
+#include <iostream>
+#include <string_view>
+
 #include "log/log_manager.h"
 
 namespace simpledb {
-FileManager& SimpleDB::GetFileManager() { return file_manager_; }
+SimpleDB::SimpleDB(std::string_view dirname)
+    : SimpleDB(dirname, BLOCK_SIZE, BUFFER_SIZE) {
+  bool is_new = file_manager_.IsNew();
+  if (is_new) {
+    std::cout << "creating new database\n";
+  } else {
+    std::cout << "recovering existing database\n";
+  }
+}
 
-LogManager& SimpleDB::GetLogManager() { return log_manager_; }
 }  // namespace simpledb
--- src/server/simpledb.h
+++ src/server/simpledb.h
@@ -1,8 +1,8 @@
 #pragma once
 
-#include <filesystem>
 #include <string_view>
 
+#include "buffer/buffer_manager.h"
 #include "file/file_manager.h"
 #include "log/log_manager.h"
 
@@ -20,28 +20,38 @@ class SimpleDB {
    * @param buff_size number of buffers in the buffer pool
    */
   SimpleDB(std::string_view dirname, int block_size, int buff_size)
-      : file_manager_(FileManager(std::filesystem::path{dirname}, block_size)),
-        log_manager_(LogManager(file_manager_, LOG_FILE)) {
-    // stop compiler warning
-    // TODO(DANG): remove this line
-    buff_size = buff_size + 1;
-  }
+      : file_manager_(dirname, block_size),
+        log_manager_(file_manager_, LOG_FILE),
+        buffer_manager_(file_manager_, log_manager_, buff_size) {}
+
+  explicit SimpleDB(std::string_view dirname);
 
   /**
    * @brief Get the file manager
    * @return a reference to the file manager
    */
-  FileManager& GetFileManager();
+  FileManager& GetFileManager() noexcept { return file_manager_; }
 
   /**
    * @brief Get the log manager
    * @return a reference to the log manager
    */
-  LogManager& GetLogManager();
+  LogManager& GetLogManager() noexcept { return log_manager_; }
+
+  /**
+   * @brief Get the buffer manager
+   * @return a reference to the buffer manager
+   */
+  BufferManager& GetBufferManager() noexcept {
+    return buffer_manager_;
+  }
 
  private:
   static constexpr std::string_view LOG_FILE{"simpledb.log"};
+  static constexpr int BLOCK_SIZE{400};
+  static constexpr int BUFFER_SIZE{8};
   FileManager file_manager_;
   LogManager log_manager_;
+  BufferManager buffer_manager_;
 };
 }  // namespace simpledb
