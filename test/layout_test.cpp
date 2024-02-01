184c8970f13c435832911c995bcd7f01318645d7
Dang Truong <truongminhdang26403@gmail.com>
2024-02-01 16:43:15 -0500

    Add unit tests for layout and record pages

---
+++ test/layout_test.cpp
@@ -0,0 +1,24 @@
+#include "record/layout.h"
+
+#include <iostream>
+
+#include "record/schema.h"
+
+namespace simpledb {
+void LayoutTest() {
+  Schema schema;
+  schema.AddIntField("A");
+  schema.AddStringField("B", 9);
+
+  Layout layout{schema};
+  for (const auto& field_name : layout.GetSchema().Fields()) {
+    int offset = layout.GetOffset(field_name);
+    std::cout << field_name << " has offset " << offset << '\n';
+  }
+}
+
+}  // namespace simpledb
+
+int main() {
+  simpledb::LayoutTest();
+}
---
+++ test/record_test.cpp
@@ -0,0 +1,69 @@
+#include <iostream>
+
+#include "record/record_page.h"
+#include "server/simpledb.h"
+
+namespace simpledb {
+void RecordTest() {
+  SimpleDB db{"recordtest", 400, 8};
+  Transaction txn{db.GetFileManager(), db.GetLogManager(),
+                  db.GetBufferManager()};
+
+  Schema schema;
+  schema.AddIntField("A");
+  schema.AddStringField("B", 9);
+  Layout layout{schema};
+  for (const auto& field_name : layout.GetSchema().Fields()) {
+    int offset = layout.GetOffset(field_name);
+    std::cout << field_name << " has offset " << offset << '\n';
+  }
+
+  BlockId block{txn.Append("testfile")};
+  txn.Pin(block);
+  RecordPage record_page{txn, block, layout};
+  record_page.Format();
+
+  std::cout << "Filling the page with random records.\n";
+  int slot = record_page.InsertAfter(-1);
+  while (slot >= 0) {
+    int n = rand() % 50;  // NOLINT(runtime/threadsafe_fn)
+    record_page.SetInt(slot, "A", n);
+    record_page.SetString(slot, "B", "rec" + std::to_string(n));
+    std::cout << "inserting into slot " << slot << ": {" << n << ", "
+              << "rec" << n << "}\n";
+    slot = record_page.InsertAfter(slot);
+  }
+
+  std::cout << "Deleting these records, whose A-values are less than 25.\n";
+  int count = 0;
+  slot = record_page.NextAfter(-1);
+  while (slot >= 0) {
+    int a = record_page.GetInt(slot, "A");
+    std::string_view b = record_page.GetString(slot, "B");
+    if (a < 25) {
+      count++;
+      std::cout << "slot " << slot << ": {" << a << ", " << b << "}\n";
+      record_page.Delete(slot);
+    }
+    slot = record_page.NextAfter(slot);
+  }
+  std::cout << count << " values under 25 were deleted.\n";
+
+  std::cout << "Here are the remaining records.\n";
+  slot = record_page.NextAfter(-1);
+  while (slot >= 0) {
+    int a = record_page.GetInt(slot, "A");
+    std::string_view b = record_page.GetString(slot, "B");
+    std::cout << "slot " << slot << ": {" << a << ", " << b << "}\n";
+    slot = record_page.NextAfter(slot);
+  }
+  txn.Unpin(block);
+  txn.Commit();
+}
+}  // namespace simpledb
+
+int main() {
+  simpledb::RecordTest();
+
+  return 0;
+}
